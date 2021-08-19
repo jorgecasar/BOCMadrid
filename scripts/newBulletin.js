90d579f447a5b3ee7f5fbceb5cec0251bd5aaaf6
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2021-08-19 13:46:01 +0200

    manage script errors

--- scripts/newBulletin.js
+++ scripts/newBulletin.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
 import { newBulletin } from '../src/newBulletinNotification.js';
 
-newBulletin();
+try {
+	newBulletin();
+} catch (e) {
+	console.error(`${e.name}: ${e.message}`);
+	process.exit(1);
+}
--- src/newBulletin.js
+++ src/newBulletin.js
@@ -7,9 +7,8 @@ export const newBulletin = async () => {
 	// Check if the latest bulletin is from current day (today).
 	const bulletingDate = new Date(date.split('T')[0]).toLocaleDateString();
 	const today = new Date().toLocaleDateString();
-	console.log(`Today ${today} / Bulletin: ${bulletingDate} - ${number}`);
 	if(bulletingDate !== today) {
-		throw new Error('Bulletin is not from current day.');
+		throw new Error(`Bulletin (${bulletingDate}) is not from current day ${today}`);
 	}
 	const total = items.length;
 	// const path = resolve(`./data/20210816.json`);
