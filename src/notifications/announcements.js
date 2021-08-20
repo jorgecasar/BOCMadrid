aec6b95280a822315b86fbb65455334b265e83b8
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2021-08-20 02:42:23 +0200

    include section 2 to announcements

--- src/notifications/announcements.js
+++ src/notifications/announcements.js
@@ -1,12 +1,15 @@
-const SECTION = "Comunidad de Madrid";
+const SECTIONS = [
+	"Comunidad de Madrid",
+	"Disposiciones y Anuncios del Estado"
+];
 
 export const announcements = (data) => data.items
-	.filter(item => item.section === SECTION)
+	.filter(item => SECTIONS.includes(item.section))
 	.map(item => ({
-			name: 'announcement',
-			params: {
-				title: item.area,
-				description: item.description,
-				file: item.files.find(file => file.type === 'pdf').url,
-			}
-		}));
+		name: 'announcement',
+		params: {
+			title: item.area,
+			description: item.description,
+			file: item.files.find(file => file.type === 'pdf').url,
+		}
+	}));
