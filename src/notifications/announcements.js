fee813007db4a087064c821ad7056ec1dd75114f
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2021-08-20 03:25:20 +0200

    add more announcements

--- src/notifications/announcements.js
+++ src/notifications/announcements.js
@@ -1,15 +1,42 @@
-const SECTIONS = [
-	"Comunidad de Madrid",
-	"Disposiciones y Anuncios del Estado"
+const section1DisabledHeadings = [
+	'Notificación'
 ];
-
-export const announcements = (data) => data.items
-	.filter(item => SECTIONS.includes(item.section))
+const section1 = (data) => data.items
+	.filter(item => item.section === 'Comunidad de Madrid')
+	.filter(item => !section1DisabledHeadings.includes(item.heading))
 	.map(item => ({
 		name: 'announcement',
 		params: {
 			title: item.area,
+			description: `${item.heading}: ${item.description}`,
+			file: item.files.find(file => file.type === 'pdf').url,
+		}
+	}));
+
+const section2 = (data) => data.items
+	.filter(item => item.section === 'Disposiciones y Anuncios del Estado')
+	.map(item => ({
+		name: 'announcement',
+		params: {
+			title: item.section,
 			description: item.description,
 			file: item.files.find(file => file.type === 'pdf').url,
 		}
 	}));
+
+const section3 = (data) => data.items
+	.filter(item => item.section === 'Administración Local Ayuntamientos')
+	.map(item => ({
+		name: 'announcement',
+		params: {
+			title: item.subsection,
+			description: item.description.replace(`${item.subsection}. `, ''),
+			file: item.files.find(file => file.type === 'pdf').url,
+		}
+	}));
+
+export const announcements = (data) => ([
+	...section1(data),
+	...section2(data),
+	...section3(data),
+]);
