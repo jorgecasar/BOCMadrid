a0560cc7df255feab7c078da5aad37102c475f37
Jorge del Casar <948953+jorgecasar@users.noreply.github.com>
2021-08-31 12:56:41 +0200

    fix capitalize roman numerals

--- src/parser/utils/titleCase.js
+++ src/parser/utils/titleCase.js
@@ -1,8 +1,13 @@
-const SMALL_WORDS = /\b(?:o|y|e|de|del|lo|el|la|lo)\b/i;
+const SMALL_WORDS = /\b(?:o|y|e|de|del|lo|el|la|lo)\b/gi;
+const ROMAN_NUMERALS = /\b(?=[ivxlcdm]+\b)(m{0,4}(?:cm|cd|d?c{0,3})(?:xc|xl|l?x{0,3})(?:ix|iv|v?i{0,3}))\b/gi;
+
 
 const capitalize = str => {
 	if (str.length) {
 		if(!SMALL_WORDS.test(str)) {
+			if(ROMAN_NUMERALS.test(str)) {
+				return str.toUpperCase();
+			}
 			return str[0].toUpperCase() + str.slice(1).toLowerCase()
 		}
 		return str.toLowerCase();
